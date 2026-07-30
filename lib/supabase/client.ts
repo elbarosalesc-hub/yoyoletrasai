f33c362eaa9ab2e4ec7294d027506edb49bb9755
elbarosalesc-hub <elba.rosalesc@gmail.com>
2026-07-30 15:17:22 -0400

    deploy(pages): publicar dashboard inmersivo correcto

--- lib/supabase/client.ts
+++
@@ -1,7 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr'
-import { getSupabasePublicConfig } from './config'
-
-export function createClient() {
-  const { url, publishableKey } = getSupabasePublicConfig()
-  return createBrowserClient(url, publishableKey)
-}
--- next-env.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// Este archivo es administrado por Next.js.
