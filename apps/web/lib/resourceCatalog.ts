a7cd36618ab36a4548201badc3170dadc74883c2
elbarosalesc-hub <elba.rosalesc@gmail.com>
2026-08-02 18:20:30 -0400

    Corregir catálogo canónico de recursos

--- apps/web/lib/resourceCatalog.ts
+++ apps/web/lib/resourceCatalog.ts
@@ -1,7 +1,10 @@
-import { getPremiumActivity, premiumActivities } from './activityCatalog'
+import { premiumActivities as coreActivities, type PremiumActivity } from './premiumActivities'
+import { extraActivities } from './extraActivities'
 
-export type { PremiumActivity } from './activityCatalog'
-export { getPremiumActivity, premiumActivities }
+export type { PremiumActivity }
+
+export const premiumActivities: PremiumActivity[] = [...coreActivities, ...extraActivities]
+export const getPremiumActivity = (slug: string) => premiumActivities.find((activity) => activity.slug === slug)
 
 export const resourceCatalog = premiumActivities
 export const getResource = getPremiumActivity
