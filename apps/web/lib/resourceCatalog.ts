88e8c628339fd385e37134f47ffc5d1f4a4263bf
elbarosalesc-hub <elba.rosalesc@gmail.com>
2026-08-02 18:20:08 -0400

    Corregir exportaciones del catálogo unificado

--- apps/web/lib/resourceCatalog.ts
+++ apps/web/lib/resourceCatalog.ts
@@ -1,6 +1,7 @@
 import { getPremiumActivity, premiumActivities } from './activityCatalog'
 
 export type { PremiumActivity } from './activityCatalog'
+export { getPremiumActivity, premiumActivities }
 
 export const resourceCatalog = premiumActivities
 export const getResource = getPremiumActivity
