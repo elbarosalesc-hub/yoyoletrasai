146d7dcef187bcccee1ebbfeabb14da44c1e6a32
elbarosalesc-hub <elba.rosalesc@gmail.com>
2026-08-03 12:02:19 -0400

    Conectar colección premium 2026 a la biblioteca

--- apps/web/lib/resourceCatalog.ts
+++ apps/web/lib/resourceCatalog.ts
@@ -1,9 +1,15 @@
 import { premiumActivities as coreActivities, type PremiumActivity } from './premiumActivities'
 import { resourceExpansion } from './resourceExpansion'
+import { premiumExpansion2026 } from './premiumExpansion2026'
 
 export type { PremiumActivity }
 
-export const premiumActivities: PremiumActivity[] = [...coreActivities, ...resourceExpansion]
+export const premiumActivities: PremiumActivity[] = [
+  ...coreActivities,
+  ...resourceExpansion,
+  ...premiumExpansion2026,
+]
+
 export const getPremiumActivity = (slug: string) => premiumActivities.find((activity) => activity.slug === slug)
 export const resourceCatalog = premiumActivities
 export const getResource = getPremiumActivity
